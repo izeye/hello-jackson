67a43991a333c66f25c5795f478d02c0b3b7c49f
Johnny Lim <izeye@naver.com>
2024-08-02 17:11:09 +0900

    Add tests for ObjectMapper ignoring exceptions
    This commit also adds Lombok dependencies for convenience.

--- src/test/java/learningtest/com/fasterxml/jackson/databind/ObjectMapperTests.java
+++ src/test/java/learningtest/com/fasterxml/jackson/databind/ObjectMapperTests.java
@@ -1,10 +1,24 @@
 package learningtest.com.fasterxml.jackson.databind;
 
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.BeanDescription;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationConfig;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.BeanPropertyWriter;
+import com.fasterxml.jackson.databind.ser.BeanSerializerModifier;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 /**
  * Tests for {@link ObjectMapper}.
@@ -33,6 +47,47 @@ class ObjectMapperTests {
     void readValueWithJsonAlias() throws JsonProcessingException {
         String json = "{\"firstNameVariant\":\"Johnny\",\"lastName\":\"Lim\"}";
         assertThat(this.mapper.readValue(json, Person.class)).isEqualTo(PERSON);
+    }
+
+    @Test
+    void writeValueAsStringWithObjectWithExceptionThrowingGetterLikeMethod() {
+        ClassWithExceptionThrowingGetterLikeMethod object = new ClassWithExceptionThrowingGetterLikeMethod("Johnny");
+        assertThatExceptionOfType(JsonMappingException.class).isThrownBy(() -> this.mapper.writeValueAsString(object));
+    }
+
+    @Test
+    void writeValueAsStringWithObjectWithExceptionThrowingGetterLikeMethodWhenExceptionIgnoringObjectMapperIsUsed() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new SimpleModule().setSerializerModifier(new BeanSerializerModifier() {
+            @Override
+            public List<BeanPropertyWriter> changeProperties(SerializationConfig config, BeanDescription beanDesc, List<BeanPropertyWriter> beanProperties) {
+                return beanProperties.stream().map((writer) -> new BeanPropertyWriter(writer) {
+                    @Override
+                    public void serializeAsField(Object bean, JsonGenerator gen, SerializerProvider prov) {
+                        try {
+                            super.serializeAsField(bean, gen, prov);
+                        }
+                        catch (Exception ignored) {
+                        }
+                    }
+                }).collect(Collectors.toList());
+            }
+        }));
+
+        ClassWithExceptionThrowingGetterLikeMethod object = new ClassWithExceptionThrowingGetterLikeMethod("Johnny");
+        assertThat(mapper.writeValueAsString(object)).isEqualTo("{\"name\":\"Johnny\"}");
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class ClassWithExceptionThrowingGetterLikeMethod {
+
+        private String name;
+
+        public String getBomb() {
+            throw new RuntimeException("Boom!");
+        }
+
     }
 
 }
